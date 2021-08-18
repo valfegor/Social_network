c61f5318fba914b82eb11bac18e47adc0417ca55
Anthony Ahumada <locopala99@gmail.com>
2021-08-18 10:55:06 +0200

    more js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -7,14 +7,15 @@ const auth = async (req,res,next) =>{
 
     jwtToken = jwtToken.split(" ")[1];
 
-    if(!jwtToken) return res.status(400).send("Sorry invalid token");
+    console.log(jwtToken);
+    if(!jwtToken) return res.status(400).send("invalid token");
 
     try {
         const payload = await jwt.verify(jwtToken,process.env.SECRET_KEY_JWT)
         req.user = payload
         next();
     } catch (e) {
-        return res.status(400).send("Authorization denied : Invalid token",e);       
+        return res.status(400).send("Authorization denied : Invalid token");       
     }
 }
 
