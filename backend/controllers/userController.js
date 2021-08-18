51054dcb11967f1137acc7a4c69c60d727af732c
Anthony Ahumada <locopala99@gmail.com>
2021-08-18 08:51:03 +0200

    More js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,6 +8,15 @@ const registerUser = async (req,res) =>{
 
     if(!req.body.name || !req.body.email || req.body.password) return res.status(400).send("Please check all te camps");
     
-    const existingEmail = 
+    const existingEmail = User.findOne({email:req.body.email});
+
+    if(existingEmail) return res.status(400).send("Sorry That email its already registered");
+
+
+    const hash = await bcrypt.hash(req.body.password,10);
+
+    const role = await Role.findOne({name:"user"});
+
+    if(!role) return res.status(400).send("")
 
 }
