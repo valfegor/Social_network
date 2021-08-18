35e5bfcde55f34168da78bce305b84edbfdc228b
Anthony Ahumada <locopala99@gmail.com>
2021-08-18 09:56:45 +0200

    more js

---
+++ backend/models/PostSchema.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    text:String,
+    hash:String,
+    status:String,
+    date:{type:Date,default:Date.now},
+    User_id:{ type: mongoose.Schema.ObjectId, ref: "user" }
+});
+
+
+const post = mongoose.model('post', postSchema);
+
+module.exports = post;
+
