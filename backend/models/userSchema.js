916995b12014f8f9b098db3df8bddefbd94b0177
Anthony Ahumada <locopala99@gmail.com>
2021-08-18 08:42:54 +0200

    more learning

---
+++ backend/models/userSchema.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const moment = require('moment');
+
+const userSchema = new mongoose.Schema({
+    name:String,
+    Email:String,
+    passwords:String,
+    Status:Boolean,
+    date:{type:Date,default:Date.now},
+    roleId: { type: mongoose.Schema.ObjectId, ref: "role" },
+});
+
+
+userSchema.methods.generateJWT = function (){
+    return jwt.sign({
+        _id:this._id,
+        name:this.name,
+        iat:moment().unix(),
+    },process.env.SECRET_KEY_JWT
+    );
+};
+
+const user = mongoose.model('user', userSchema);
+
+
+module.exports = user;
